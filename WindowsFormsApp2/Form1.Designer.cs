e9a61971fe87cc9e89ff518fe6aa82a21b09145a
ahmed-morshedy <ahmedmorshedy270@yahoo.com>
2024-11-12 02:00:54 +0200

    first commit

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace ImageSlideShowApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPrevious.Location = new System.Drawing.Point(126, 368);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(139, 48);
+            this.btnPrevious.TabIndex = 2;
+            this.btnPrevious.Text = "Previous";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNext.Location = new System.Drawing.Point(512, 368);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(139, 48);
+            this.btnNext.TabIndex = 3;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(126, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(525, 277);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.UseWaitCursor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrevious);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace ImageSlideShowApp
+{
+    public partial class Form1 : Form
+    {
+        // List to store the paths of images
+        private List<string> imagePaths;
+        private int currentIndex = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadImages();
+            ShowImage();
+        }
+
+        private void LoadImages()
+        {
+            // Use the "images" folder within the project's output directory
+            string imagesFolder = System.IO.Path.Combine(Application.StartupPath, "images");
+
+            // Initialize the list with the paths to the images in the "images" folder
+            imagePaths = new List<string>
+    {
+        System.IO.Path.Combine(imagesFolder, "1729514005359-543649132.jpg"),
+        System.IO.Path.Combine(imagesFolder, "1729513941011-75960478.jpg"),
+        System.IO.Path.Combine(imagesFolder, "1729514161955-940403391.jpg"),
+        System.IO.Path.Combine(imagesFolder, "1729514244552-890562009.jpg"),
+        System.IO.Path.Combine(imagesFolder, "1729514327995-438255237.jpg"),
+       
+        // Add more image filenames as needed
+    };
+        }
+
+        private void ShowImage()
+        {
+            if (imagePaths.Count > 0)
+            {
+                try
+                {
+                    pictureBox1.ImageLocation = imagePaths[currentIndex];
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error loading image: " + ex.Message, "Image Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            if (imagePaths.Count > 0)
+            {
+                currentIndex--;
+                if (currentIndex < 0)
+                {
+                    currentIndex = imagePaths.Count - 1; // Loop back to the last image
+                }
+                ShowImage();
+            }
+        }
+
+      
+        private void btnNext_Click_1(object sender, EventArgs e)
+        {
+            if (imagePaths.Count > 0)
+            {
+                currentIndex++;
+                if (currentIndex >= imagePaths.Count)
+                {
+                    currentIndex = 0; // Loop back to the first image
+                }
+                ShowImage();
+            }
+
+        }
+    }
+}
